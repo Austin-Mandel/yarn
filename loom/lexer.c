f35505e4b1413f97359ebc4c5068825842adadea
whatisthisiamnotgoodwithcomputer <comi.jake0@gmail.com>
2014-10-16 23:20:09 -0400

    Fixed my previous commit, forgot to git add -A. Also forgot to move pick and bat, now moved.

--- loom/lexer.c
+++
@@ -1,4 +0,0 @@
-#include "lexer.h"
-const Evaluator EVALUATOR_INIT = {NOPRE, 0, NOPOST, NOOP, 0};
-const Statement STATEMENT_INIT = {NOPRE, 0, NOPOST, VOID, 0};
-
--- loom/utils.h
+++
@@ -1,8 +0,0 @@
-#ifndef __LOOM_UTILS_H
-#define __LOOM_UTILS_H
-#include "lexer.h"
-
-void printStatement(Statement* statement);
-void printAssignmentOperator(AssignmentOperator op);
-
-#endif
